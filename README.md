cccbb804f0a08dfc31462775a64b1b77913be5f6
daboynb <106079917+daboynb@users.noreply.github.com>
2023-12-22 12:21:12 +0100

    -.-

---
+++ README.md
@@ -0,0 +1,39 @@
+# Play integrity fix
+
+# Download from 
+https://github.com/daboynb/PlayIntegrityNEXT/releases/tag/Release
+
+# Why this fork?
+It downloads the fp automatically.
+
+# Instructions
+
+1) Download the zip
+2) Download the apk
+3) Install the apk and grant root permissions
+4) Flash the zip and reboot
+
+When the FP will be banned, you will only need to open the app and then check for Play Integrity Attestation. The module will download the new PIF.json. 
+
+# ATTENTION
+Sometimes, you may need to double-check for Play Integrity Attestation if the new PIF.json was just downloaded.
+It can happen if you use apps like Yasnac. Currently, I have not encountered this bug using the Google Play developer check.
+
+# Demonstration
+
+https://github.com/daboynb/PlayIntegrityNEXT/assets/106079917/2f5b8376-aa3e-4be1-91d7-5f003ac72499
+
+
+# What does the APK do
+
+    [ -e /data/adb/pif.json ] && (echo "rm"; rm /data/adb/pif.json) || echo "no"
+
+    sleep 04 && if pgrep -f com.google.android.gms > /dev/null; then pkill -f com.google.android.gms; else echo "Process is not running."; fi && if pgrep -f com.google.android.gms.unstable > /dev/null; then pkill -f com.google.android.gms.unstable; else echo "Process is not running."; fi
+
+The apk was made with tasker, this is the profile:
+
+https://github.com/daboynb/PlayIntegrityNEXT/blob/main/Gms.tsk.xml
+
+https://play.google.com/store/apps/details?id=net.dinglisch.android.taskerm
+
+https://play.google.com/store/apps/details?id=net.dinglisch.android.appfactory
---
+++ app/src/main/java/es/chiteroman/playintegrityfix/EntryPoint.java
@@ -0,0 +1,110 @@
+package es.chiteroman.playintegrityfix;
+
+import android.os.Build;
+import android.util.JsonReader;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.lang.reflect.Field;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.KeyStoreSpi;
+import java.security.Provider;
+import java.security.Security;
+import java.util.HashMap;
+import java.util.Map;
+
+public final class EntryPoint {
+    private static final Map<String, String> map = new HashMap<>();
+
+    public static void init(String data) {
+        try (JsonReader reader = new JsonReader(new StringReader(data))) {
+            reader.beginObject();
+            while (reader.hasNext()) {
+                map.put(reader.nextName(), reader.nextString());
+            }
+            reader.endObject();
+        } catch (IOException e) {
+            LOG("Couldn't read JSON from Zygisk: " + e);
+            return;
+        }
+        spoofProvider();
+        spoofDevice();
+    }
+
+    private static void spoofProvider() {
+        final String KEYSTORE = "AndroidKeyStore";
+
+        try {
+            Provider provider = Security.getProvider(KEYSTORE);
+            KeyStore keyStore = KeyStore.getInstance(KEYSTORE);
+
+            Field f = keyStore.getClass().getDeclaredField("keyStoreSpi");
+            f.setAccessible(true);
+            CustomKeyStoreSpi.keyStoreSpi = (KeyStoreSpi) f.get(keyStore);
+            f.setAccessible(false);
+
+            CustomProvider customProvider = new CustomProvider(provider);
+            Security.removeProvider(KEYSTORE);
+            Security.insertProviderAt(customProvider, 1);
+
+            LOG("Spoof KeyStoreSpi and Provider done!");
+
+        } catch (KeyStoreException e) {
+            LOG("Couldn't find KeyStore: " + e);
+        } catch (NoSuchFieldException e) {
+            LOG("Couldn't find field: " + e);
+        } catch (IllegalAccessException e) {
+            LOG("Couldn't change access of field: " + e);
+        }
+    }
+
+    static void spoofDevice() {
+        setProp("PRODUCT", map.get("PRODUCT"));
+        setProp("DEVICE", map.get("DEVICE"));
+        setProp("MANUFACTURER", map.get("MANUFACTURER"));
+        setProp("BRAND", map.get("BRAND"));
+        setProp("MODEL", map.get("MODEL"));
+        setProp("FINGERPRINT", map.get("FINGERPRINT"));
+        setVersionProp("SECURITY_PATCH", map.get("SECURITY_PATCH"));
+    }
+
+    private static void setProp(String name, String value) {
+        if (name == null || value == null || name.isEmpty() || value.isEmpty()) return;
+        try {
+            Field field = Build.class.getDeclaredField(name);
+            field.setAccessible(true);
+            String oldValue = (String) field.get(null);
+            field.set(null, value);
+            field.setAccessible(false);
+            if (value.equals(oldValue)) return;
+            LOG(String.format("[%s]: %s -> %s", name, oldValue, value));
+        } catch (NoSuchFieldException e) {
+            LOG(String.format("Couldn't find '%s' field name.", name));
+        } catch (IllegalAccessException e) {
+            LOG(String.format("Couldn't modify '%s' field value.", name));
+        }
+    }
+
+    private static void setVersionProp(String name, String value) {
+        if (name == null || value == null || name.isEmpty() || value.isEmpty()) return;
+        try {
+            Field field = Build.VERSION.class.getDeclaredField(name);
+            field.setAccessible(true);
+            String oldValue = (String) field.get(null);
+            field.set(null, value);
+            field.setAccessible(false);
+            if (value.equals(oldValue)) return;
+            LOG(String.format("[%s]: %s -> %s", name, oldValue, value));
+        } catch (NoSuchFieldException e) {
+            LOG(String.format("Couldn't find '%s' field name.", name));
+        } catch (IllegalAccessException e) {
+            LOG(String.format("Couldn't modify '%s' field value.", name));
+        }
+    }
+
+    static void LOG(String msg) {
+        Log.d("PIF/Java", msg);
+    }
+}
