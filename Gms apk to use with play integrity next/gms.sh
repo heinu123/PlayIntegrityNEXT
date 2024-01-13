b9a0bc280284be3cc795defce8a922ba441b47a6
daboynb <you@example.com>
2024-01-13 13:02:11 +0100

    Update gms.sh

--- Gms apk to use with play integrity next/gms.sh
+++ Gms apk to use with play integrity next/gms.sh
@@ -1,4 +1,3 @@
-#!/data/data/com.termux/files/home/local/bin/bash
-#!/data/data/com.termux/files/usr/bin/bash
-su -c "rm /data/adb/pif.json"
-su -c "{ if pgrep -f com.google.android.gms > /dev/null; then pkill -f com.google.android.gms && echo "com.google.android.gms process killed."; else echo "com.google.android.gms process is not running."; fi; } && { if pgrep -f com.google.android.gms.unstable > /dev/null; then pkill -f com.google.android.gms.unstable && echo "com.google.android.gms.unstable process killed."; else echo "com.google.android.gms.unstable process is not running, no need to kill."; fi; }"
+su
+rm /data/adb/pif.json"
+{ if pgrep -f com.google.android.gms > /dev/null; then pkill -f com.google.android.gms && echo "com.google.android.gms process killed."; else echo "com.google.android.gms process is not running."; fi; } && { if pgrep -f com.google.android.gms.unstable > /dev/null; then pkill -f com.google.android.gms.unstable && echo "com.google.android.gms.unstable process killed."; else echo "com.google.android.gms.unstable process is not running, no need to kill."; fi; }"
