c9fde7536830808a72bb97674646e42c3b281678
daboynb <106079917+daboynb@users.noreply.github.com>
2024-01-13 14:31:44 +0100

    Update gms.sh

--- Gms apk to use with play integrity next/gms.sh
+++ Gms apk to use with play integrity next/gms.sh
@@ -1,24 +1,35 @@
+if [ -f "/storage/emulated/0/gms_termux.log" ]; then
+  rm "/storage/emulated/0/gms_termux.log" 
+  
 touch /storage/emulated/0/gms_termux.log
 
-su -c 'if [ -f /data/adb/pif.json ]; then
-  rm "/data/adb/pif.json" && echo "File /data/adb/pif.json removed."
-else
-  echo "Pif.json not present, let's continue"
-fi' >> /storage/emulated/0/gms_termux.log
+su -c '{
+  if [ -f /data/adb/pif.json ]; then
+    rm "/data/adb/pif.json"
+    echo "File /data/adb/pif.json removed."
+  else
+    echo "pif.json not present, let's continue"
+  fi
+}' >> /storage/emulated/0/gms_termux.log
 
-su -c 'if pgrep -f com.google.android.gms > /dev/null; then
-  pkill -f com.google.android.gms
-  echo "com.google.android.gms process killed."
-else
-  echo "com.google.android.gms process is not running."
-fi' >> /storage/emulated/0/gms_termux.log
+su -c '{
+  if pgrep -f com.google.android.gms > /dev/null; then
+    pkill -f com.google.android.gms
+    echo "com.google.android.gms process killed."
+  else
+    echo "com.google.android.gms process is not running."
+  fi
+}' >> /storage/emulated/0/gms_termux.log
 
-su -c 'if pgrep -f com.google.android.gms.unstable > /dev/null; then
-  pkill -f com.google.android.gms.unstable
-  echo "com.google.android.gms.unstable process killed."
-else
-  echo "com.google.android.gms.unstable process is not running, no need to kill."
-fi' >> /storage/emulated/0/gms_termux.log
+su -c '{
+  if pgrep -f com.google.android.gms.unstable > /dev/null; then
+    pkill -f com.google.android.gms.unstable
+    echo "com.google.android.gms.unstable process killed." 
+  else
+    echo "com.google.android.gms.unstable process is not running, no need to kill." 
+  fi
+}' >> /storage/emulated/0/gms_termux.log
 
 cat /storage/emulated/0/gms_termux.log
+
 # rm /storage/emulated/0/gms_termux.log
