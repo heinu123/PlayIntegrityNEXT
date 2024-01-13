02c8af8b96731b1405c38934c9a9f5b64e740844
daboynb <you@example.com>
2024-01-13 12:55:09 +0100

    Delete gms.sh

--- Gms apk to use with play integrity next/gms.sh
+++
@@ -1,2 +0,0 @@
-su -c "rm /data/adb/pif.json"
-su -c "{ if pgrep -f com.google.android.gms > /dev/null; then pkill -f com.google.android.gms && echo "com.google.android.gms process killed."; else echo "com.google.android.gms process is not running."; fi; } && { if pgrep -f com.google.android.gms.unstable > /dev/null; then pkill -f com.google.android.gms.unstable && echo "com.google.android.gms.unstable process killed."; else echo "com.google.android.gms.unstable process is not running, no need to kill."; fi; }"
