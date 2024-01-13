913b1acb4af5b3d2c0b52ea5d4616c7beb8abe48
daboynb <you@example.com>
2024-01-13 14:12:42 +0100

    Update gms.sh

--- Gms apk to use with play integrity next/gms.sh
+++ Gms apk to use with play integrity next/gms.sh
@@ -3,27 +3,28 @@ su -c '{
     rm "/data/adb/pif.json"
     echo "File /data/adb/pif.json removed."
   else
-    echo "Continue"
+    echo "Continue" > /storage/emulated/0/gms_termux.log
   fi
 }'
 
 su -c '{
   if pgrep -f com.google.android.gms > /dev/null; then
     pkill -f com.google.android.gms
-    echo "com.google.android.gms process killed."
+    echo "com.google.android.gms process killed." >> /storage/emulated/0/gms_termux.log
   else
-    echo "com.google.android.gms process is not running."
+    echo "com.google.android.gms process is not running." >> /storage/emulated/0/gms_termux.log
   fi
 }'
 
 su -c '{
   if pgrep -f com.google.android.gms.unstable > /dev/null; then
     pkill -f com.google.android.gms.unstable
-    echo "com.google.android.gms.unstable process killed."
+    echo "com.google.android.gms.unstable process killed." >> /storage/emulated/0/gms_termux.log
   else
-    echo "com.google.android.gms.unstable process is not running, no need to kill."
+    echo "com.google.android.gms.unstable process is not running, no need to kill." >> /storage/emulated/0/gms_termux.log
   fi
 }'
 
-su -c "pgrep -f com.google.android.gms.unstable && echo unstable"    
-su -c "pgrep -f com.google.android.gms echo gms"
+cat >> /storage/emulated/0/gms_termux.log
+
+rm >> /storage/emulated/0/gms_termux.log
