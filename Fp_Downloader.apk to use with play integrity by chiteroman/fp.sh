6bf094fd187ead5f0839724474b291e9b575e5b9
daboynb <106079917+daboynb@users.noreply.github.com>
2024-01-13 15:01:50 +0100

    Update fp.sh

--- Fp_Downloader.apk to use with play integrity by chiteroman/fp.sh
+++ Fp_Downloader.apk to use with play integrity by chiteroman/fp.sh
@@ -1,4 +1,4 @@
-#su -c "cd /storage/emulated/0 && /data/adb/modules/playcurl/curl -L "https://raw.githubusercontent.com/daboynb/PlayIntegrityNEXT/main/Gms%20apk%20to%20use%20with%20play%20integrity%20next/gms.sh" -o gms.sh && clear && /system/bin/sh gms.sh"
+#su -c "cd /storage/emulated/0 && /data/adb/modules/playcurl/curl -L "https://raw.githubusercontent.com/daboynb/PlayIntegrityNEXT/main/Fp_Downloader.apk%20to%20use%20with%20play%20integrity%20by%20chiteroman/fp.sh" -o gms.sh && clear && /system/bin/sh gms.sh"
 
 su 
 {
