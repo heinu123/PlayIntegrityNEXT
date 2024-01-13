f63a13a7a129fcd84b671dd92d064b359dfc0e51
daboynb <you@example.com>
2024-01-13 13:02:58 +0100

    Update gms.sh

--- Gms apk to use with play integrity next/gms.sh
+++ Gms apk to use with play integrity next/gms.sh
@@ -1,3 +1,3 @@
 su
-rm /data/adb/pif.json"
+rm "/data/adb/pif.json"
 { if pgrep -f com.google.android.gms > /dev/null; then pkill -f com.google.android.gms && echo "com.google.android.gms process killed."; else echo "com.google.android.gms process is not running."; fi; } && { if pgrep -f com.google.android.gms.unstable > /dev/null; then pkill -f com.google.android.gms.unstable && echo "com.google.android.gms.unstable process killed."; else echo "com.google.android.gms.unstable process is not running, no need to kill."; fi; }"
