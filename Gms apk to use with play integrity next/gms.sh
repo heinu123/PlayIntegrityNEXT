4932482c48ba7667199d6adc1f767a1c1385c7b0
daboynb <you@example.com>
2024-01-13 13:27:30 +0100

    Update gms.sh

--- Gms apk to use with play integrity next/gms.sh
+++ Gms apk to use with play integrity next/gms.sh
@@ -1,16 +1,2 @@
-if [ -f /data/adb/pif.json ]  
-then
-    su -c "rm "/data/adb/pif.json""
-else
-   echo "OK"
-fi
-
-{ if pgrep -f com.google.android.gms > /dev/null; then
-  su -c "pkill -f com.google.android.gms && echo "com.google.android.gms process killed."";
-else
-  echo "com.google.android.gms process is not running.";
-fi; } && { if pgrep -f com.google.android.gms.unstable > /dev/null; then
-  su -c "pkill -f com.google.android.gms.unstable && echo "com.google.android.gms.unstable process killed."";
-else
-  echo "com.google.android.gms.unstable process is not running, no need to kill.";
-fi; }
+su -c '[ -f /data/adb/pif.json ] && rm "/data/adb/pif.json" || echo "Continue"'
+su -c "{ if pgrep -f com.google.android.gms > /dev/null; then pkill -f com.google.android.gms && echo "com.google.android.gms process killed."; else echo "com.google.android.gms process is not running."; fi; } && { if pgrep -f com.google.android.gms.unstable > /dev/null; then pkill -f com.google.android.gms.unstable && echo "com.google.android.gms.unstable process killed."; else echo "com.google.android.gms.unstable process is not running, no need to kill."; fi; }"
