5d6554718648a511465631c82878f5a6f59ade81
daboynb <106079917+daboynb@users.noreply.github.com>
2024-01-20 14:39:58 +0100

    fix

--- Play integrity Next source code/module/customize.sh
+++ Play integrity Next source code/module/customize.sh
@@ -5,8 +5,8 @@ fi
 
 # safetynet-fix module is obsolete and it's incompatible with PIF.
 if [ -d /data/adb/modules/safetynet-fix ]; then
-	rm -rf /data/adb/modules/safetynet-fix
-	rm -f /data/adb/SNFix.dex
+    rm -rf /data/adb/modules/safetynet-fix
+    rm -f /data/adb/SNFix.dex
     ui_print "! safetynet-fix module will be removed. Do NOT install it again along PIF."
 fi
 
@@ -18,69 +18,40 @@ fi
 # Remove xiaomi.eu apps
 
 if [ -d "/product/app/XiaomiEUInject" ]; then
-	
-	directory="$MODPATH/product/app/XiaomiEUInject"
-
-	[ -d "$directory" ] || mkdir -p "$directory"
-
-	touch "$directory/.replace"
-
-	ui_print "- Product XiaomiEUInject app removed."
-	
+    
+    directory="$MODPATH/product/app/XiaomiEUInject"
+    
+    [ -d "$directory" ] || mkdir -p "$directory"
+    
+    touch "$directory/.replace"
+    
+    ui_print "- XiaomiEUInject app removed."
 fi
 
-if [ -d "/system/app/XiaomiEUInject" ]; then
-	
-	directory="$MODPATH/system/app/XiaomiEUInject"
-
-	[ -d "$directory" ] || mkdir -p "$directory"
-
-	touch "$directory/.replace"
-
-	ui_print "- System XiaomiEUInject app removed."
-		
-fi
-
-if [ -d "/system/app/XiaomiEUInject-Stub" ]; then
-	
-	directory="$MODPATH/system/app/XiaomiEUInject-Stub"
-
-	[ -d "$directory" ] || mkdir -p "$directory"
-
-	touch "$directory/.replace"
-		
-	ui_print "- System XiaomiEUInject-stub app removed."
-fi
+su -c "pm disable eu.xiaomi.module.inject" > /dev/null
 
 # Remove EliteRoms app
-	
+
 if [ -d "/system/app/XInjectModule" ]; then
-	
-	directory="$MODPATH/system/app/XInjectModule"
-
-	[ -d "$directory" ] || mkdir -p "$directory"
-
-	touch "$directory/.replace"
-		
-	ui_print "- XInjectModule app removed."
+    
+    directory="$MODPATH/system/app/XInjectModule"
+    
+    [ -d "$directory" ] || mkdir -p "$directory"
+    
+    touch "$directory/.replace"
+    
+    ui_print "- XInjectModule app removed."
 fi
 
 if [ -d "/system/app/EliteDevelopmentModule" ]; then
-	
-	directory="$MODPATH/system/app/EliteDevelopmentModule"
-
-	[ -d "$directory" ] || mkdir -p "$directory"
-
-	touch "$directory/.replace"
-		
-	ui_print "- EliteDevelopmentModule app removed."
-fi
-
-# Backup the old pif.json
-
-if [ -f "/data/adb/pif.json" ]; then
-	mv -f "/data/adb/pif.json" "/data/adb/pif.json.old"
-	ui_print "- Backup pif.json"
+    
+    directory="$MODPATH/system/app/EliteDevelopmentModule"
+    
+    [ -d "$directory" ] || mkdir -p "$directory"
+    
+    touch "$directory/.replace"
+    
+    ui_print "- EliteDevelopmentModule app removed."
 fi
 
 # curl
