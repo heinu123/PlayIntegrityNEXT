bb6469813143b8c5c7e0f2535d0c52f74a0551f7
daboynb <106079917+daboynb@users.noreply.github.com>
2024-01-22 19:31:15 +0100

    update sh

--- Fp_Downloader.apk to use with play integrity by chiteroman/fp.sh
+++ Fp_Downloader.apk to use with play integrity by chiteroman/fp.sh
@@ -1,15 +1,24 @@
 #su -c "cd /storage/emulated/0 && /system/bin/curl -L "https://raw.githubusercontent.com/daboynb/PlayIntegrityNEXT/main/Fp_Downloader.apk%20to%20use%20with%20play%20integrity%20by%20chiteroman/fp.sh" -o fp.sh && /system/bin/sh fp.sh"
 
+echo "Deleting old pif.json"
 rm -f "/data/adb/pif.json" > /dev/null 
+echo
 
+echo "Downloading the pif.json"
 /system/bin/curl -o /data/adb/pif.json https://raw.githubusercontent.com/daboynb/autojson/main/pif.json
+echo
 
+echo "Killing com.google.android.gms"
 pkill -f com.google.android.gms > /dev/null 
+echo
 
+echo "Killing com.google.android.gms.unstable"
 pkill -f com.google.android.gms.unstable > /dev/null 
+echo
 
 if [ -e /data/adb/pif.json ]; then 
     echo "Pif.json downloaded succesfully"
 else 
     echo "Pif.json not present, something went wrong."
 fi
+echo
--- Gms apk to use with play integrity next/gms.sh
+++ Gms apk to use with play integrity next/gms.sh
@@ -1,7 +1,13 @@
 #su -c "cd /storage/emulated/0 && /data/adb/modules/playcurl/curl -L "https://raw.githubusercontent.com/daboynb/PlayIntegrityNEXT/main/Gms%20apk%20to%20use%20with%20play%20integrity%20next/gms.sh" -o gms.sh && /system/bin/sh gms.sh"
 
+echo "Deleting old pif.json"
 rm -f "/data/adb/pif.json" > /dev/null 
+echo
 
+echo "Killing com.google.android.gms"
 pkill -f com.google.android.gms > /dev/null 
+echo
 
+echo "Killing com.google.android.gms.unstable"
 pkill -f com.google.android.gms.unstable > /dev/null 
+echo
