fbc92576c747e8505a3c003b66989c947aa20764
daboynb <106079917+daboynb@users.noreply.github.com>
2024-01-22 14:43:17 +0100

    Update gms.sh

--- Gms apk to use with play integrity next/gms.sh
+++ Gms apk to use with play integrity next/gms.sh
@@ -1,30 +1,7 @@
 #su -c "cd /storage/emulated/0 && /data/adb/modules/playcurl/curl -L "https://raw.githubusercontent.com/daboynb/PlayIntegrityNEXT/main/Gms%20apk%20to%20use%20with%20play%20integrity%20next/gms.sh" -o gms.sh && /system/bin/sh gms.sh"
 
-su 
+rm -f "/data/adb/pif.json" > /dev/null 
 
-{
-  if [ -f /data/adb/pif.json ]; then
-    rm "/data/adb/pif.json"
-    echo "File /data/adb/pif.json removed." >> /storage/emulated/0/gms.log
-  else
-    echo "Continue" >> /storage/emulated/0/gms.log
-  fi
-}
+pkill -f com.google.android.gms > /dev/null 
 
-{
-  if pgrep -f com.google.android.gms > /dev/null; then
-    pkill -f com.google.android.gms
-    echo "com.google.android.gms process killed." >> /storage/emulated/0/gms.log
-  else
-    echo "com.google.android.gms process is not running." >> /storage/emulated/0/gms.log
-  fi
-}
-
-{
-  if pgrep -f com.google.android.gms.unstable > /dev/null; then
-    pkill -f com.google.android.gms.unstable
-    echo "com.google.android.gms.unstable process killed." >> /storage/emulated/0/gms.log
-  else
-    echo "com.google.android.gms.unstable process is not running, no need to kill." >> /storage/emulated/0/gms.log
-  fi
-}
+pkill -f com.google.android.gms.unstable > /dev/null 
