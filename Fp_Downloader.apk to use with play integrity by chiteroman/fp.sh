716fe5e6fe6325523f389ddba87a508ba95e724b
daboynb <106079917+daboynb@users.noreply.github.com>
2024-01-23 23:10:56 +0100

    revert last commit

--- Fp_Downloader.apk to use with play integrity by chiteroman/fp.sh
+++ Fp_Downloader.apk to use with play integrity by chiteroman/fp.sh
@@ -1,31 +1,31 @@
-#su -c "cd /storage/emulated/0 && /system/bin/curl -L \"https://raw.githubusercontent.com/daboynb/PlayIntegrityNEXT/main/Fp_Downloader.apk%20to%20use%20with%20play%20integrity%20by%20chiteroman/fp.sh\" -o fp.sh && /system/bin/sh fp.sh"
+#su -c "cd /storage/emulated/0 && /system/bin/curl -L "https://raw.githubusercontent.com/daboynb/PlayIntegrityNEXT/main/Fp_Downloader.apk%20to%20use%20with%20play%20integrity%20by%20chiteroman/fp.sh" -o fp.sh && /system/bin/sh fp.sh"
 
 clear
 echo
-echo -e "\033[1;32m[+] Deleting old pif.json\033[0m"
-rm -f "/data/adb/pif.json" > /dev/null
+echo "[+] Deleting old pif.json"
+rm -f "/data/adb/pif.json" > /dev/null 
 echo
 
-echo -e "\033[1;32m[+] Check if the miui eu inject module is present\033[0m"
-pm disable eu.xiaomi.module.inject > /dev/null 2>&1 && echo -e "\033[1;32mThe miui eu inject module is disabled. Please reboot to ensure that the modification takes effect.\033[0m" || true
+echo "[+] Check if the miui eu inject module is present"
+pm disable eu.xiaomi.module.inject > /dev/null 2>&1 && echo "The miui eu inject module is disabled. Please reboot to ensure that the modification takes effect." || true
 echo
 
-echo -e "\033[1;32m[+] Downloading the pif.json\033[0m"
+echo "[+] Downloading the pif.json"
 /system/bin/curl -o /data/adb/pif.json http://tinyurl.com/autojson > /dev/null 2>&1 || /system/bin/curl -o /data/adb/pif.json http://tinyurl.com/autojson
 echo
 
-echo -e "\033[1;32m[+] Killing com.google.android.gms\033[0m"
-pkill -f com.google.android.gms > /dev/null
+echo "[+] Killing com.google.android.gms"
+pkill -f com.google.android.gms > /dev/null 
 echo
 
-echo -e "\033[1;32m[+] Killing com.google.android.gms.unstable\033[0m"
-pkill -f com.google.android.gms.unstable > /dev/null
+echo "[+] Killing com.google.android.gms.unstable"
+pkill -f com.google.android.gms.unstable > /dev/null 
 echo
 
-if [ -e /data/adb/pif.json ]; then
-    echo -e "\033[1;32m[+] Pif.json downloaded successfully\033[0m"
-else
-    echo -e "\033[1;32m[+] Pif.json not present, something went wrong.\033[0m"
+if [ -e /data/adb/pif.json ]; then 
+    echo "[+] Pif.json downloaded succesfully"
+else 
+    echo "[+] Pif.json not present, something went wrong."
 fi
 
 rm "$0"
