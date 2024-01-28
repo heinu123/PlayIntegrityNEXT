586ad68845019e4401cfdffe1f2d94bdf88d530f
daboynb <106079917+daboynb@users.noreply.github.com>
2024-01-28 17:55:46 +0100

    Update boot.sh

--- Kernel  name changer/boot.sh
+++ Kernel  name changer/boot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Deps
+# Dependencies
 sudo apt install zip
 sudo apt install lz4
 
@@ -17,18 +17,22 @@ Choose an option:  "
     case $ans in
 
     1)
+        # Variables
         read -e -p "Drag & drop your boot.img : " boot_file
         eval boot_file="$boot_file"
         
         output_file="boot_modified.img"
 
+        # Make sure that the output will be of the same size
         while true; do
             read -e -p "Word to remove: " search_string
             read -e -p "Word to replace: " replace_string
 
+            # Count the number of words
             search_length=$(echo -n "$search_string" | wc -c)
             replace_length=$(echo -n "$replace_string" | wc -c)
 
+            # Checks
             if [ "$search_string" == "$replace_string" ]; then
                 echo "Error: Both strings are the same. Please enter different strings."
             elif [ "$search_length" -ne "$replace_length" ]; then
@@ -39,6 +43,7 @@ Choose an option:  "
             fi
         done
 
+        # Replace the words
         if strings "$boot_file" | grep -q "$search_string"; then
             sed "s/$search_string/$replace_string/g" "$boot_file" > "$output_file"
             echo "The process has been completed."
@@ -49,23 +54,30 @@ Choose an option:  "
 
         ;;
     2)
+        # Variables
         read -e -p "Drag & drop your kernel zip  : " zip_kernel 
         eval zip_kernel="$zip_kernel"
 
+        # Copy kernel.zip
         cp "$zip_kernel" . > /dev/null 2>&1
 
+        # Unzip kernel.zip
         unzip $zip_kernel -d out
 
+        # Copy image.lz4
         cp out/Image.lz4 .
         rm out/Image.lz4
 
+        # Make sure that the output will be of the same size
         while true; do
             read -e -p "Word to remove: " search_string
             read -e -p "Word to replace: " replace_string
 
+            # Count the number of words
             search_length=$(echo -n "$search_string" | wc -c)
             replace_length=$(echo -n "$replace_string" | wc -c)
 
+            # Checks
             if [ "$search_string" == "$replace_string" ]; then
                 echo "Error: Both strings are the same. Please enter different strings."
             elif [ "$search_length" -ne "$replace_length" ]; then
@@ -76,6 +88,7 @@ Choose an option:  "
             fi
         done
 
+        # Replace the word
         if strings "Image.lz4" | grep -q "$search_string"; then
             lz4 -c -d Image.lz4 | sed "s/$search_string/$replace_string/g" | lz4 -c -l -12 --favor-decSpeed - > Image-patched.lz4 
             echo "The process has been completed."
@@ -85,14 +98,13 @@ Choose an option:  "
             exit
         fi
 
+        # Build the new xip
         mv Image-patched.lz4 out/Image.lz4 
-
         cd out; zip -r ../KERNEL.zip *
-
         cd ..
 
+        # Clean
         rm Image.lz4
-        
         rm -rf out
         ;;
     0)      
