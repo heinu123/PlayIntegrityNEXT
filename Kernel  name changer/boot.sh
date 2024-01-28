e036b3274c2617ed324d9975dfab6cdb4ba99dc9
daboynb <106079917+daboynb@users.noreply.github.com>
2024-01-28 16:55:14 +0100

    Update boot.sh

--- Kernel  name changer/boot.sh
+++ Kernel  name changer/boot.sh
@@ -24,12 +24,13 @@ Choose an option:  "
         read -e -p "Word to replace (lineage for example) : " replace_string
 
         if strings "$boot_file" | grep -q "$search_string"; then
-            perl -pe "print if s/$search_string/$replace_string/g" "$boot_file" > "$output_file"
+            sed "s/$search_string/$replace_string/g" "$boot_file" > "$output_file"
             echo "The process has been completed."
             echo "The final modified file has been saved as: $output_file"
         else
             echo "Failed, that boot.img does not contain that word"
         fi
+
         ;;
     2)
         read -e -p "Drag & drop your kernel zip  : " zip_kernel 
