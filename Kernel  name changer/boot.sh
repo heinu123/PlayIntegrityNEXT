cbc3626714ea3c2ceb59afabfbe022f43f40df76
daboynb <106079917+daboynb@users.noreply.github.com>
2024-01-28 13:54:25 +0100

    Update boot.sh

--- Kernel  name changer/boot.sh
+++ Kernel  name changer/boot.sh
@@ -8,7 +8,10 @@ output_file="$HOME/boot_modified.img"
 read -e -p "Word to remove (lineage for example) : " search_string
 read -e -p "Word to replace (lineage for example) : " replace_string
 
-perl -pe "s/$search_string/$replace_string/g" "$boot_file" > "$output_file"
-
-echo "The process has been completed."
-echo "The final modified file has been saved as: $output_file"
+if strings "$boot_file" | grep -q "$search_string"; then
+    perl -pe "print if s/$search_string/$replace_string/g" "$boot_file" > "$output_file"
+    echo "The process has been completed."
+    echo "The final modified file has been saved as: $output_file"
+else
+    echo "Failed, that boot.img does not contain that word"
+fi
