29dc2060545f1cb97064b24bc93aebc1498c781f
daboynb <106079917+daboynb@users.noreply.github.com>
2024-02-04 19:56:56 +0100

    improved kernel name detection, added comments

--- Fp_Downloader.apk to use with play integrity by chiteroman/fp.sh
+++ Fp_Downloader.apk to use with play integrity by chiteroman/fp.sh
@@ -1,28 +1,29 @@
 #su -c "cd /storage/emulated/0 && /system/bin/curl -L "https://raw.githubusercontent.com/daboynb/PlayIntegrityNEXT/main/Fp_Downloader.apk%20to%20use%20with%20play%20integrity%20by%20chiteroman/fp.sh" -o fp.sh && /system/bin/sh fp.sh"
 
-# Checks
+# Check is the setup is correct
 if [ -f "/data/adb/magisk/busybox" ]; then
     if /data/adb/magisk/busybox cat /data/adb/modules/playintegrityfix/module.prop | /data/adb/magisk/busybox grep -q 'NEXT'; then
-        echo "Wrong setup! Rtfm!"
+        echo "Wrong setup! Remove play integrity fix next and download the official chiteroman module! Playcurl is meant to be used alongside pif official"
         exit
     fi
 fi
 
 if [ -f "/data/adb/ksu/bin/busybox" ]; then
     if /data/adb/ksu/bin/busybox cat /data/adb/modules/playintegrityfix/module.prop | /data/adb/magisk/busybox grep -q 'NEXT'; then
-        echo "Wrong setup! Rtfm!"
+        echo "Wrong setup! Remove play integrity fix next and download the official chiteroman module! Playcurl is meant to be used alongside pif official"
         exit
     fi
 fi
 
 if [ -f "/data/adb/ap/bin/busybox" ]; then
     if /data/adb/ap/bin/busybox cat /data/adb/modules/playintegrityfix/module.prop | /data/adb/magisk/busybox grep -q 'NEXT'; then
-        echo "Wrong setup! Rtfm!"
+        echo "Wrong setup! Remove play integrity fix next and download the official chiteroman module! Playcurl is meant to be used alongside pif official"
         exit
     fi
 fi
 # End of checks
 
+# Delete outdated pif.json
 echo
 echo "[+] Deleting old pif.json"
 
@@ -42,38 +43,56 @@ then
 fi
 echo
 
+# Disable problematic packages
 echo "[+] Check if inject apks are present"
 pm disable eu.xiaomi.module.inject > /dev/null 2>&1 && echo "The miui eu inject apk is disabled now. YOU NEED TO REBOOT OR YOU WON'T BE ABLE TO PASS DEVICE INTEGRITY!." || true
 pm disable com.goolag.pif > /dev/null 2>&1 && echo "The Evolution X inject apk is disabled now. YOU NEED TO REBOOT OR YOU WON'T BE ABLE TO PASS DEVICE INTEGRITY!." || true
 pm disable com.lineageos.pif > /dev/null 2>&1 && echo "The Lineage inject apk is disabled now. YOU NEED TO REBOOT OR YOU WON'T BE ABLE TO PASS DEVICE INTEGRITY!." || true
 echo
 
+# Download pif.json
 echo "[+] Downloading the pif.json"
-
 if [ -f /data/adb/modules/playintegrityfix/migrate.sh ]; then
     /system/bin/curl -L "https://raw.githubusercontent.com/daboynb/autojson/main/pif.json" -o /data/adb/modules/playintegrityfix/custom.pif.json > /dev/null 2>&1 || /system/bin/curl -L "https://raw.githubusercontent.com/daboynb/autojson/main/pif.json" -o /data/adb/modules/playintegrityfix/custom.pif.json
 else
     /system/bin/curl -L "https://raw.githubusercontent.com/daboynb/autojson/main/pif.json" -o /data/adb/pif.json > /dev/null 2>&1 || /system/bin/curl -L "https://raw.githubusercontent.com/daboynb/autojson/main/pif.json" -o /data/adb/pif.json
 fi
+echo 
 
+# Kill gms processes
 echo "[+] Killing com.google.android.gms"
 pkill -f com.google.android.gms > /dev/null 
 echo
 
+# Kill gms processes
 echo "[+] Killing com.google.android.gms.unstable"
 pkill -f com.google.android.gms.unstable > /dev/null 
 echo
 
+# Check if the pif is present
 if [ -f /data/adb/pif.json ] || [ -f /data/adb/modules/playintegrityfix/custom.pif.json ]; then
     echo "[+] Pif.json downloaded successfully"
 else
     echo "[+] Pif.json is not present, something went wrong."
 fi
 
-echo
-echo "[+] Kernel:"
-echo
-uname -r
-echo
+# Check if the kernel name is banned
+kernel_name=$(uname -r)
+
+# Banned kernels names from https://xdaforums.com/t/module-play-integrity-fix-safetynet-fix.4607985/post-89308909
+banned_names=("aicp" "arter97" "blu_spark" "cm" "crdroid" "cyanogenmod" "deathly" "eas" "elementalx" "elite" "franco" "lineage" "lineageos" "noble" "optimus" "slimroms" "sultan")
+
+banned=false
+
+for keyword in "${banned_names[@]}"; do
+    if echo "$kernel_name" | grep -iq "$keyword"; then
+        echo "Your kernel name \"$keyword\" is banned"
+        banned=true
+    fi
+done
+
+if [ "$banned" = false ]; then
+    echo "Your kernel name is not banned"
+fi
 
 rm "$0"
