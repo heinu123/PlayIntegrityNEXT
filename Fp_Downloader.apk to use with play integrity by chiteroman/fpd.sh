2eeac6fa8d3a933696b2b128b3a0244676cb7c61
daboynb <106079917+daboynb@users.noreply.github.com>
2024-02-05 08:35:26 +0100

    add feedback

--- Fp_Downloader.apk to use with play integrity by chiteroman/fpd.sh
+++ Fp_Downloader.apk to use with play integrity by chiteroman/fpd.sh
@@ -12,17 +12,30 @@ elif [ -f "$ap_busybox" ]; then
     busybox_type="$ap_busybox"
 fi
 
+echo Your busybox is "$busybox_type"
+
 # Modules path
 modules_dir="/data/adb/modules"
 
 # Disable other modules for testing incompatibility 
+disabled_modules=()
 for subdir in "$modules_dir"/*/; do
     if [ -d "$subdir" ]; then
         module_prop="$subdir/module.prop"
         
         if [ -f "$module_prop" ]; then
             module_name=$("$busybox_type" grep -E 'name=Zygisk Next|name=Play Integrity Fix|name=playcurl' "$module_prop")            
-            [ -z "$module_name" ] && touch "$subdir/disable"
+            if [ -z "$module_name" ]; then
+                touch "$subdir/disable"
+                disabled_module_name=$(basename "$subdir")
+                disabled_modules+=("$disabled_module_name")
+            fi
         fi
     fi
 done
+
+if [ ${#disabled_modules[@]} -gt 0 ]; then
+    echo "Disabled modules: ${disabled_modules[@]}"
+else
+    echo "No modules disabled."
+fi
