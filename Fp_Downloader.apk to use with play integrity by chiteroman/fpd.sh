f9c29da6c2e4df8ebc762cc8867bf00cf42e21bc
daboynb <106079917+daboynb@users.noreply.github.com>
2024-02-14 21:31:59 +0100

    Update fpd.sh

--- Fp_Downloader.apk to use with play integrity by chiteroman/fpd.sh
+++ Fp_Downloader.apk to use with play integrity by chiteroman/fpd.sh
@@ -112,34 +112,13 @@ for keyword in "${banned_names[@]}"; do
     fi
 done
 
-# Modules path
-modules_dir="/data/adb/modules"
-
-# Disable other modules for testing incompatibility 
-disabled_modules=()
-for subdir in "$modules_dir"/*/; do
-    if [ -d "$subdir" ]; then
-        module_prop="$subdir/module.prop"
-        
-        if [ -f "$module_prop" ]; then
-            module_name=$("$busybox_path" grep -E 'name=Zygisk Next|name=Play Integrity Fix|name=playcurl' "$module_prop")            
-            if [ -z "$module_name" ]; then
-                touch "$subdir/disable"
-                disabled_module_name=$(basename "$subdir")
-                disabled_modules+=("$disabled_module_name")
-            fi
-        fi
-    fi
+# Disable other modules for testing incompatibility
+list="$("$busybox_path" find /data/adb/modules/* -prune -type d)"
+for module in $list; do
+    touch "$module/disable"
 done
-
-# Print the result
-if [ ${#disabled_modules[@]} -gt 0 ]; then
-    echo "[+] Disabled modules: ${disabled_modules[@]}"
-    sleep 04
-    reboot
-else
-    echo "[+] No modules disabled."
-fi
 
 # Auto delete the script
 rm "$0"
+
+reboot
